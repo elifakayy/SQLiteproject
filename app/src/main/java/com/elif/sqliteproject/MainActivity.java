c07a16d3d205a3055ebb4e1d3f7cd965a7baacfa
elif <eelifaakay@gmail.com>
2023-04-26 16:15:41 +0300

    Create SQLite database and add data

--- app/src/main/java/com/elif/sqliteproject/MainActivity.java
+++ app/src/main/java/com/elif/sqliteproject/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         try {
 
             SQLiteDatabase database=this.openOrCreateDatabase("painters",MODE_PRIVATE,null);
