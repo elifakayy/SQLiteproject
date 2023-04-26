b873da99e3c7da093eea9b3b487eee9839dfc71b
elif <eelifaakay@gmail.com>
2023-04-26 16:27:52 +0300

    Delete and update

--- app/src/main/java/com/elif/sqliteproject/MainActivity.java
+++ app/src/main/java/com/elif/sqliteproject/MainActivity.java
@@ -17,17 +17,23 @@ public class MainActivity extends AppCompatActivity {
         try {
 
             SQLiteDatabase database=this.openOrCreateDatabase("painters",MODE_PRIVATE,null);
-            database.execSQL("CREATE TABLE IF NOT EXISTS painters(name VARCHAR,picture VARCHAR)");
+            database.execSQL("CREATE TABLE IF NOT EXISTS painters(id INTEGER PRIMARY KEY, name VARCHAR,picture VARCHAR)");
             //database.execSQL("INSERT INTO painters (name,picture) VALUES('Leonardo Da Vinci', 'Mona Lisa')");
-            database.execSQL("INSERT INTO painters (name,picture) VALUES('Johannes Vermeer', 'İnci Küpeli Kız')");
+            //database.execSQL("INSERT INTO painters (name,picture) VALUES('Johannes Vermeer', 'İnci Küpeli Kız')");
+            //database.execSQL("UPDATE painters SET picture ='Son Akşam Yemeği' WHERE name= 'Leonardo Da Vinci'");
+            //database.execSQL("DELETE FROM painters WHERE id=2");
+
             Cursor cursor = database.rawQuery("SELECT * FROM painters",null);
 
             int nameIx =cursor.getColumnIndex("name");
             int pictureIx=cursor.getColumnIndex("picture");
+            int idIx=cursor.getColumnIndex("id");
+
             while (cursor.moveToNext())
             {
                 System.out.println("Name : "+cursor.getString(nameIx));
                 System.out.println("Picture : "+cursor.getString(pictureIx));
+                System.out.println("ID : "+cursor.getString(idIx));
             }cursor.close();
         } catch (Exception e){
             e.printStackTrace();
